900d0118f15f561d9360597429b413e31bd22396
wr447329532 <2527154253@qq.com>
2025-09-19 14:11:33 +0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import AttendanceSystem from './AttendanceSystem';
+
+function App() {
+  return (
+    <div className="App">
+      <AttendanceSystem />
+    </div>
+  );
+}
+
+export default App;
